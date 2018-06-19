9fca0112d78f6380e4729bcb8a9fba20daab2a0b
Luke Whitten <whittenluke@gmail.com>
2018-06-19 11:00:39 -0400

    nextjs commit

---
+++ pages/index.js
@@ -0,0 +1,234 @@
+import Head from 'next/head'
+
+export default () =>
+
+
+
+	<div>
+		<Head>
+      <title>Learnatic</title>
+
+			<link rel="shortcut icon" href="favicon.ico"/>
+
+			<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,600,400italic,700' rel='stylesheet' type='text/css'/>
+
+
+			<link rel="stylesheet" href="/static/css/animate.css"/>
+
+			<link rel="stylesheet" href="/static/css/flexslider.css"/>
+
+			<link rel="stylesheet" href="/static/css/icomoon.css"/>
+
+			<link rel="stylesheet" href="/static/css/magnific-popup.css"/>
+
+			<link rel="stylesheet" href="/static/css/bootstrap.css"/>
+
+
+			<link rel="stylesheet" href="/static/css/style.css"/>
+    </Head>
+	<div id="fh5co-page">
+		<section id="fh5co-header">
+			<div class="container">
+				<nav role="navigation">
+					<ul class="pull-left left-menu">
+						<li><a href="about.html">About</a></li>
+						<li><a href="tour.html">Tour</a></li>
+					</ul>
+					<h1 id="fh5co-logo"><a href="index.html">
+						<img src="/static/images/logo2.png"/></a></h1>
+					<ul class="pull-right right-menu">
+						<li><a href="#">Login</a></li>
+						<li class="fh5co-cta-btn"><a href="#">Sign up</a></li>
+					</ul>
+				</nav>
+			</div>
+		</section>
+
+
+		<section id="fh5co-hero" class="js-fullheight" data-next="yes">
+			<div class="fh5co-overlay"></div>
+			<div class="container">
+				<div class="fh5co-intro js-fullheight">
+					<div class="fh5co-intro-text">
+
+						<div class="fh5co-center-position">
+							<h3 class="animate-box">Free Online FUN Games and Education Resources <br/> for Kids and Parents</h3>
+
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="fh5co-learn-more animate-box">
+				<a href="#" class="scroll-btn">
+					<span class="text">Start Learning!</span>
+					<span class="arrow"><i class="icon-chevron-down"></i></span>
+				</a>
+			</div>
+		</section>
+
+
+
+
+		<section id="fh5co-features">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="shrink"><div class="fh5co-feature">
+							<div class="fh5co-icon">
+								<a href="0-1.html"><img src="/static/images/icon0-1.png"/></a>
+							</div>
+							<h3>0-1 year old</h3>
+							<p>sample copy</p>
+						</div></div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="shrink"><div class="fh5co-feature">
+							<div class="fh5co-icon">
+								<a href="1-3.html"><img src="/static/images/icon1-3.png"/></a>
+							</div>
+							<h3>1-3 years old</h3>
+							<p>sample copy</p>
+						</div></div>
+					</div>
+					<div class="clearfix visible-sm-block"></div>
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="shrink"><div class="fh5co-feature">
+							<div class="fh5co-icon">
+								<a href="3-5.html"><img src="/static/images/icon3-5.png"/></a>
+							</div>
+							<h3>3-5 years old</h3>
+							<p>sample copy</p>
+						</div></div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="shrink"><div class="fh5co-feature">
+							<div class="fh5co-icon">
+								<a href="5-8.html"><img src="/static/images/icon5-8.png"/></a>
+							</div>
+							<h3>5-8 years old</h3>
+							<p>sample copy</p>
+						</div>
+					</div></div>
+				</div>
+				<div class="row">
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="shrink"><div class="fh5co-feature">
+							<div class="fh5co-icon">
+								<a href="8-10.html"><img src="/static/images/icon8-10.png"/></a>
+							</div>
+							<h3>8-10 years old</h3>
+							<p>sample copy</p>
+						</div>
+					</div></div>
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="shrink"><div class="fh5co-feature">
+							<div class="fh5co-icon">
+								<a href="10-12.html"><img src="/static/images/icon10-12.png"/></a>
+							</div>
+							<h3>10-12 years old</h3>
+							<p>sample copy</p>
+						</div></div>
+					</div>
+					<div class="clearfix visible-sm-block"></div>
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="shrink"><div class="fh5co-feature">
+							<div class="fh5co-icon">
+								<a href="12-15.html"><img src="/static/images/icon12-15.png"/></a>
+							</div>
+							<h3>12-15 years old</h3>
+							<p>sample copy</p>
+						</div>
+					</div></div>
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="shrink"><div class="fh5co-feature">
+							<div class="fh5co-icon">
+								<a href="15-18.html"><img src="/static/images/icon15-18.png"/></a>
+							</div>
+							<h3>15-18 years old</h3>
+							<p>sample copy</p>
+						</div></div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+
+
+		<section id="fh5co-subscribe">
+			<div class="container">
+
+				<h3 class="animate-box"><label for="email">Subscribe to Our Email List for Updates!</label></h3>
+				<form action="#" method="post" class="animate-box">
+					<i class="fh5co-icon icon-paper-plane"></i>
+					<input type="email" class="form-control" placeholder="Enter your email" id="email" name="email"/>
+					<input type="submit" value="Send" class="btn btn-primary"/>
+				</form>
+
+			</div>
+		</section>
+
+
+		<footer id="fh5co-footer">
+			<div class="container">
+				<div class="row row-bottom-padded-md">
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="fh5co-footer-widget">
+							<h3>Company</h3>
+							<ul class="fh5co-links">
+								<li><a href="#">About Us</a></li>
+
+								<li><a href="#">Tour</a></li>
+
+								<li><a href="#">Team</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="fh5co-footer-widget">
+							<h3>Support</h3>
+							<ul class="fh5co-links">
+								<li><a href="#">Terms of Use</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="fh5co-footer-widget">
+							<h3>Contact Us</h3>
+							<p>
+								<a href="mailto:learnatic.us@gmail.com">learnatic.us@gmail.com</a> <br/>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-3 col-sm-6 col-xs-12 animate-box">
+						<div class="fh5co-footer-widget">
+							<h3>Follow Us</h3>
+							<ul class="fh5co-social">
+								<li><a href="#"><i class="icon-twitter"></i></a></li>
+								<li><a href="#"><i class="icon-facebook"></i></a></li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+			<div class="fh5co-copyright animate-box">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							Shared by <i class="fa fa-love"></i><a href="https://bootstrapthemes.co">BootstrapThemes</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+	</div>
